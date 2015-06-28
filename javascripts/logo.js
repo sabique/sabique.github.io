89e4c75aaa1d3fe5fe9d80145e39d9cd9c2d432f
Sabique A Khan <sabiquekhan@gmail.com>
2015-06-28 16:09:57 +0530

    Create logo.js

---
+++ javascripts/logo.js
@@ -0,0 +1,30 @@
+
+    var ctx = document.getElementById("canvas").getContext("2d");
+    	
+// #layer1
+	ctx.save();
+	ctx.transform(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, -1002.362200);
+	
+// #path8068
+	ctx.beginPath();
+	ctx.globalAlpha = 1.0;
+	ctx.lineJoin = 'miter';
+	ctx.strokeStyle = 'rgb(255, 255, 255)';
+	ctx.miterLimit = 4;
+	ctx.lineWidth = 2.800930;
+	ctx.fillStyle = 'rgb(104, 178, 177)';
+	ctx.arc(25.000000, 1027.362200, 22.599535, 0.000000, 6.28318531, 1);
+	ctx.fill();
+	ctx.stroke();
+	
+// #text8070
+	ctx.save();
+	ctx.transform(0.927326, 0.000000, 0.000000, 1.078370, 0.000000, 0.000000);
+	ctx.lineJoin = 'miter';
+	ctx.lineCap = 'butt';
+	ctx.lineWidth = 1.000000;
+	ctx.fillStyle = 'rgb(255, 255, 255)';
+	ctx.font = "normal normal 28.71992874px Pristina";
+	ctx.fillText("Sak", 5.060549, 962.614990);
+	ctx.restore();
+	ctx.restore();
