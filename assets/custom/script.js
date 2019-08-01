c93237518db7eb695d581d29b019b5b38ff5a175
Sabique <sabiquekhan@gmail.com>
2019-08-01 23:30:14 +0530

    Added method to calculate and render the experience years

---
+++ assets/custom/script.js
@@ -0,0 +1,14 @@
+function calculateExperience(){
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const currentMonth = date.getMonth();
+
+    return (currentMonth === 1)? `${currentYear - 2016} years` : `${currentYear - 2016}.${currentMonth - 1} years`;
+}
+
+function renderExperience(){
+    const exp = document.querySelector('strong.experience');
+    exp.textContent = calculateExperience();
+}
+
+renderExperience();
